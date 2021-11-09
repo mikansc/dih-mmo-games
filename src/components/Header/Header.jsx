e48f9d372badb9ced9c29187ee4516aa875fc6ea
Michael Nascimento <michael.nsc@outlook.com>
2021-11-09 11:11:59 -0300

    Atualiza Header e adiciona links

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import Container from "../Layout/Container";
 
 import { HeaderContainer } from "./Header.styles";
 
+import LightLogo from "../../assets/img/logo-light.png";
+import { Link } from "react-router-dom";
+import { Button } from "../Button/Button";
+
 export const Header = () => {
-  return <HeaderContainer>Header</HeaderContainer>;
+  return (
+    <HeaderContainer>
+      <Container fullWidth style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+        <Link to="/">
+          <img src={LightLogo} alt="MMO Games Free to Play - By Mika Nascimento" />
+        </Link>
+        <nav>
+          <Button as={Link} variant="link" to="/news">
+            Notícias
+          </Button>
+          <Button as={Link} variant="link" to="/games">
+            Jogos
+          </Button>
+        </nav>
+      </Container>
+    </HeaderContainer>
+  );
 };
--- src/components/Header/Header.styles.js
+++ src/components/Header/Header.styles.js
@@ -3,11 +3,29 @@ import styled from "styled-components";
 export const HeaderContainer = styled.header`
   width: 100%;
   height: 80px;
+
   position: fixed;
   top: 0;
   left: 0;
 
+  display: flex;
+  align-items: center;
+
   box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
 
   background: linear-gradient(to top, #243b55, #141e30);
+  color: ${({ theme }) => theme.color.defaultText};
+
+  & img {
+    width: 140px;
+  }
+
+  & nav {
+    & a {
+      text-transform: uppercase;
+      font-weight: 700;
+      font-size: 0.75rem;
+      margin-right: ${({ theme }) => theme.spacing(3)};
+    }
+  }
 `;
