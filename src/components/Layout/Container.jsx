00f907b1fe6bdea4b0cd3d44cff50c1734336d13
Michael Nascimento <michael.nsc@outlook.com>
2021-11-11 09:54:10 -0300

    Adiciona prop Paper no Container.jsx

--- src/components/Layout/Container.jsx
+++ src/components/Layout/Container.jsx
@@ -1,19 +1,21 @@
 import styled, { css } from "styled-components";
 
-const Container = ({ children, fullWidth, ...rest }) => {
+const Container = ({ children, fullWidth, paper, ...rest }) => {
   return (
-    <StyledContainer fullWidth={fullWidth} {...rest}>
+    <StyledContainer fullWidth={fullWidth} {...rest} paper={paper}>
       {children}
     </StyledContainer>
   );
 };
 
 const StyledContainer = styled.div`
-  ${({ theme, fullWidth }) => css`
+  ${({ theme, fullWidth, paper }) => css`
     width: 100%;
     max-width: ${fullWidth ? "100%" : theme.screen.xl};
     padding: ${theme.spacing(1)};
     margin: 0 auto;
+    background-color: ${paper ? '#FFF' : "transparent"};
+    color: ${paper ? theme.color.primary.dark : "inherit"};
   `}
 `;
 
