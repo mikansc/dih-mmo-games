ba43f7fd8fc9ee28fb0c8fc73e547a46bb265bac
Michael Nascimento <michael.nsc@outlook.com>
2021-11-11 11:56:11 -0300

    Refatora TextInput para usar icones

--- src/components/TextInput/TextInput.jsx
+++ src/components/TextInput/TextInput.jsx
@@ -1,6 +1,6 @@
 import { InputGroup } from "./TextInput.styles";
 
-export const TextInput = ({ id, label, name, error, helperText, ...rest }) => {
+export const TextInput = ({ id, label, name, error, helperText,Icon, onClick, ...rest }) => {
   if (!name) {
     throw new Error("Campo de texto sem prop 'name'!");
   }
@@ -8,6 +8,7 @@ export const TextInput = ({ id, label, name, error, helperText, ...rest }) => {
   return (
     <InputGroup error={!!error}>
       {!!label && <label htmlFor={id || name}>{label}</label>}
+      {!!Icon && <button type="button" onClick={onClick}>{<Icon />}</button>}
       <input id={id || name} type="text" {...rest} />
       {!!helperText && (<span>{helperText}</span>)}
     </InputGroup>
--- src/components/TextInput/TextInput.styles.js
+++ src/components/TextInput/TextInput.styles.js
@@ -1,33 +1,55 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export const InputGroup = styled.div`
-  margin-bottom: ${({theme})=> theme.spacing(1)};
-  margin-top: ${({theme})=> theme.spacing(1)};
+  ${({ theme, error }) => css`
+    margin-bottom: ${theme.spacing(1)};
+    margin-top: ${theme.spacing(1)};
+    background-color: ${!!error ? theme.color.error.light : "transparent"};
+    color: ${!!error ? theme.color.error.dark : theme.color.primary.dark};
+    border: 1px solid ${!!error ? theme.color.error.dark : theme.color.primary.dark};
+  `}
+
+  width: 100%;
   display: flex;
   flex-direction: column;
   position: relative;
+  border-radius: 5px;
+  padding: 0.2em 0.5em 0.8em;
 
-  
+
   & label {
     font-weight: 400;
-    margin-left: 0.85em;
     margin-bottom: 0.25em;
-    font-size: .95rem;
-    color: ${({error, theme}) => !!error ? theme.color.error.dark : theme.color.primary.dark};
+    font-size: .70rem;
+    color: ${({ error, theme }) => !!error ? theme.color.error.dark : theme.color.primary.dark};
   }
-  
+
   & input {
-    background-color: ${({error, theme}) => !!error ? theme.color.error.light : 'transparent'};
-    color: ${({error, theme}) => !!error ? theme.color.error.dark : theme.color.primary.dark};
-    border: 1px solid ${({error, theme}) => !!error ? theme.color.error.dark : theme.color.primary.dark};
-    border-radius: 5px;
-    padding: .85em;
-    width: 100%;
+    border: none;
+    outline: none;
   }
-  
+
   & span {
     margin-top: 0.75em;
-    color: ${({error, theme}) => !!error ? theme.color.error.dark : theme.color.primary.dark};
+    color: ${({ error, theme }) => !!error ? theme.color.error.dark : theme.color.primary.dark};
     font-size: 1rem;
   }
-`
+
+  & button {
+    ${({theme})=> css`
+      padding-right: ${theme.spacing(1)};
+      padding-left: ${theme.spacing(1)};
+    `}
+    
+    position: absolute;
+    top: 0;
+    right: 0;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: none;
+    background-color: transparent;
+    font-size: 1.5rem;
+  }
+`;
