9eb18538d12652ae276a89fd5adb3a8aff0e7b75
Michael Nascimento <michael.nsc@outlook.com>
2021-11-11 20:58:46 -0300

    Adiciona string-utils para corte de textos longos

--- src/utils/string-utils.js
+++ src/utils/string-utils.js
@@ -1,3 +1,7 @@
-export function parseTitle(string) {
+export function parseQuoteCodes(string) {
   return string.replaceAll("\u2018", "'").replaceAll("\u2019", "'");
+}
+
+export function limitText(text, limit = 140) {
+  return text.slice(0, limit - 3).concat('...')
 }
