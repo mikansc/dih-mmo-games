5979f74fdb4f7f6ef8d03b2bb62674ee74b89c41
Michael Nascimento <michael.nsc@outlook.com>
2021-11-11 20:58:32 -0300

    Implementa componente Card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,5 +1,17 @@
 import React from "react";
+import { limitText } from "../../utils/string-utils";
+import { GameCardArticle, GameCardMedia } from "./Card.styles";
 
-export const Card = () => {
-  return <div>Card</div>;
+export const Card = ({ item, onClick }) => {
+  return (
+    <GameCardArticle onClick={onClick} style={{ maxWidth: "380px" }}>
+      <GameCardMedia>
+        <img src={item.thumbnail} alt={item.title} />
+        <div>
+          <h4>{item.title}</h4>
+        </div>
+      </GameCardMedia>
+      <span>{limitText(item.short_description)}</span>
+    </GameCardArticle>
+  );
 };
---
+++ src/components/Card/Card.styles.js
@@ -0,0 +1,64 @@
+import styled from "styled-components";
+
+export const GameCardArticle = styled.article`
+  margin: 1rem 0;
+  max-width: 350px;
+  height: 340px;
+  overflow: hidden;
+  padding: 0.5rem;
+  border-radius: 5px;
+  cursor: pointer;
+  border: 1px solid ${({ theme }) => theme.color.gray.main};
+  transition: all 100ms linear;
+
+  &:hover {
+    transform: translate(3px, -3px);
+    box-shadow: ${({ theme }) => theme.shadown};
+  }
+`;
+
+export const GameCardMedia = styled.div`
+  width: 100%;
+  margin-bottom: 0.75rem;
+  overflow: hidden;
+  position: relative;
+
+  & > div {
+    position: absolute;
+    width: 100%;
+    top: 0;
+    bottom: 0;
+    background-color: rgba(20, 30, 48, 0.8);
+    z-index: 1000;
+
+    opacity: 0;
+    transition: opacity 180ms ease-in-out;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding-bottom: 0.25rem;
+
+    & > h4 {
+      font-size: 1.25rem;
+      text-transform: uppercase;
+      color: ${({ theme }) => theme.color.primary.text};
+      transform: translateY(100%);
+      transition: transform 200ms ease-in-out;
+    }
+  }
+
+  &:hover > div {
+    opacity: 1;
+
+    & > h4 {
+      transform: translateY(0);
+    }
+  }
+
+  & img {
+    width: 100%;
+    display: block;
+    height: auto;
+  }
+`;
