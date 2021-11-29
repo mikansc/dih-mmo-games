79e03bed000b2a73d43cae5532f6aa3f58c51a36
Michael Nascimento <michael.nsc@outlook.com>
2021-11-29 17:31:14 -0300

    Cria hook useSearchFilter

---
+++ src/hooks/useSearchFilter.js
@@ -0,0 +1,22 @@
+import * as React from 'react'
+import { useEffect, useState } from "react";
+
+export const useSearchFilter = (data) => {
+  const [term, setTerm] = useState("");
+  const [filtered, setFiltered] = useState(data);
+
+  useEffect(() => {
+    setFiltered(data);
+  }, [data]);
+
+  useEffect(() => {
+    const filteredList = data.filter(news => new RegExp(term, "ig").test(news.title));
+    setFiltered(filteredList);
+  }, [data, term]);
+
+  const handleSearch = React.useCallback((event) => {
+    setTerm(event.target.value);
+  },[])
+
+  return {filtered, searchTerm: term, handleSearch}
+}
